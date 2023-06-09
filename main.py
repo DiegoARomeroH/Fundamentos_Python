661f4874180e28f8a83011bdc16e8cdb49e9f581
DiegoARomeroH <diegoalonsoromerohernandez@gmail.com>
2023-06-09 16:22:02 -0600

    02 Variables

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hola Mundo")
+print("Soy Alumno de TIADSM")
+
+print("Podemos enviar diferentes tipos de dato")
+print(12)
+print(12.25)
+print(True)
+print([])
---
+++ variable.py
@@ -0,0 +1,15 @@
+# Python es no tipado
+
+name = "Diego"
+lastname = "Romero"
+age = 19
+height = 1.80
+is_student = True
+groups = ("3A", "3B", "3C",)
+
+# function type 
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(groups))
