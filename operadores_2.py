1c450ef24663f319b8b1fff8539090588d194f89
DiegoARomeroH <diegoalonsoromerohernandez@gmail.com>
2023-06-09 17:00:36 -0600

    05 Operadores booleanos y logicos

---
+++ operadores_2.py
@@ -0,0 +1,19 @@
+# Operadores booleanos
+# == (Igualdad)
+print(1 == 1)
+# != (distinto)
+print(1 != 2)
+# > (Mayor que)
+print(4 > 2)
+# < (Menor que)
+print(5 < 10)
+# operadores logicos
+# and 
+print(True and False)
+print(False and True)
+# or 
+print(False or True)
+print(False or True)
+# not 
+print(not True)
+
