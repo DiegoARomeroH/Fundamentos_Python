eaf9c779d408ce061524d9c3ff81ba86cb0e7e8b
DiegoARomeroH <diegoalonsoromerohernandez@gmail.com>
2023-06-15 09:43:24 -0600

    07 Strings

--- condicionales.py
+++ condicionales.py
@@ -13,9 +13,11 @@ else:
     print ("Es Mayor de Edad")
 
 age = 75
+
 if age < 18:
     print ("Es Menor de Edad")
 elif age < 70:
      print ("Es Mayor de Edad")
 else:
     print("Es adulto Mayor")
+    
